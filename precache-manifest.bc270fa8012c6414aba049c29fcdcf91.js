e1bc839e69e1db48f4e7f934ebfb774a0715f6f5
sunnysingha911 <sunnysingha911@gmail.com>
2020-09-11 04:32:07 +0530

    Updates

---
+++ precache-manifest.bc270fa8012c6414aba049c29fcdcf91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466df0f2b1f9519b4b3a43fe8c8fda85",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "3b64b14255208851afe3",
+    "url": "/Todo/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f71c9c623b6e27665933",
+    "url": "/Todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3b64b14255208851afe3",
+    "url": "/Todo/static/js/2.057246fb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Todo/static/js/2.057246fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f71c9c623b6e27665933",
+    "url": "/Todo/static/js/main.1d3e6a39.chunk.js"
+  },
+  {
+    "revision": "01487ddd545745f7acc3",
+    "url": "/Todo/static/js/runtime-main.c45df924.js"
+  }
+]);
